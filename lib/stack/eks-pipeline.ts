864f76a2cae9279cc07c7f19bd9f7310bcba5dc7
Shivam Rastogi <shivam.riky@gmail.com>
2024-02-17 18:06:44 -0800

    Remove other stages

--- lib/stack/eks-pipeline.ts
+++ lib/stack/eks-pipeline.ts
@@ -36,8 +36,6 @@ export default class PipelineConstruct extends Construct {
     .wave({
         id: "envs",
         stages: [
-          { id: "dev", stackBuilder: blueprint.clone('us-west-2')},
-          { id: "test", stackBuilder: blueprint.clone('us-east-2')},
           { id: "prod", stackBuilder: blueprint.clone('us-east-1')}
         ]
       })
